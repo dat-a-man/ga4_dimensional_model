a131d191a3cffb162332de19e68ea78170fdba33
dat-a-man <98139823+dat-a-man@users.noreply.github.com>
2023-11-17 11:21:50 +0000

    update dim_traffic_source

--- ga4_dm/models/dimensions/dim_traffic_source.sql
+++ ga4_dm/models/dimensions/dim_traffic_source.sql
@@ -4,5 +4,7 @@ SELECT
     traffic_source__name,
     traffic_source__medium,
     traffic_source__source
+    -- "Alena, create a macro that allows users to specify their paid sources. For instance, if Reddit ads are marked as organic by Google, the user can define them as paid, and the macro will adjust the custom_source field accordingly." 
+    -- case when traffic_source = "X" then 'paid' else 'organic' as custom_source
 FROM
 {{ ref("stg_events")}}
